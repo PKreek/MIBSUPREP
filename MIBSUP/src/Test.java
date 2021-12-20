c422a9cac4e46fe719c8a215dd96d475963a39fa
PKreek <patrick.kreek@gmail.com>
2021-12-20 12:32:15 +0100

    NyTest

--- MIBSUP/src/Test.java
+++ MIBSUP/src/Test.java
@@ -12,7 +12,7 @@ public class Test {
     
     public static void main(String[] args){
         
-    System.out.println("Hello World");
+    System.out.println("Hello Noah, Niklas and Patrick");
     
     }
 }
