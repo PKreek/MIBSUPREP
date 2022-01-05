5b1040bf1732a39d7ff476af3a1a144f1f32d82c
NikAntKar <niklas.karlsson12345@hotmail.com>
2022-01-05 19:59:44 +0100

    blablla

--- MIBSUP/src/InloggadAdmin.java
+++ MIBSUP/src/InloggadAdmin.java
@@ -54,6 +54,7 @@ public class InloggadAdmin extends javax.swing.JFrame {
         btnRegistreraAgent = new javax.swing.JButton();
         btnSetOmradesChef = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
+        btnSetKontorschef = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -202,6 +203,18 @@ public class InloggadAdmin extends javax.swing.JFrame {
             }
         });
 
+        btnSetKontorschef.setBackground(new java.awt.Color(33, 31, 31));
+        btnSetKontorschef.setFont(new java.awt.Font("SansSerif", 0, 16)); // NOI18N
+        btnSetKontorschef.setForeground(new java.awt.Color(153, 153, 153));
+        btnSetKontorschef.setText("Ändra kontorschef");
+        btnSetKontorschef.setBorder(null);
+        btnSetKontorschef.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnSetKontorschef.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSetKontorschefActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
@@ -235,7 +248,8 @@ public class InloggadAdmin extends javax.swing.JFrame {
                             .addComponent(btnSokAgent)
                             .addComponent(btnAndraLosen)
                             .addComponent(btnRegistreraAgent)
-                            .addComponent(jButton1))
+                            .addComponent(jButton1)
+                            .addComponent(btnSetKontorschef))
                         .addGap(0, 0, Short.MAX_VALUE))))
         );
         jPanel2Layout.setVerticalGroup(
@@ -266,8 +280,10 @@ public class InloggadAdmin extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnSetOmradesChef)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnSetKontorschef)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnAndraLosen)
-                .addContainerGap(20, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -284,70 +300,19 @@ public class InloggadAdmin extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnAndraLosenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAndraLosenActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        try {
+            // TODO add your handling code here:
+            new ÄndraInformationAgent(idb).setVisible(true);
+        } catch (InfException ex) {
+            Logger.getLogger(InloggadAdmin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void btnSetOmradesChefActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSetOmradesChefActionPerformed
         // TODO add your handling code here:
-        new ÄndraLösenord(anvandarnamn, losenord, idb, anvandare).setVisible(true);
-    }//GEN-LAST:event_btnAndraLosenActionPerformed
-
-    private void btnRegistreraAlienActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistreraAlienActionPerformed
-        try {
-            // TODO add your handling code here:
-            new RegistreraAlien(anvandarnamn, idb).setVisible(true);
-        } catch (InfException ex) {
-            Logger.getLogger(InloggadAdmin.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }//GEN-LAST:event_btnRegistreraAlienActionPerformed
-
-    private void btnTaBortAlienActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTaBortAlienActionPerformed
-        new TaBortAlien(idb).setVisible(true);
-        // TODO add your handling code here:
-    }//GEN-LAST:event_btnTaBortAlienActionPerformed
-
-    private void btnSattAdminActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSattAdminActionPerformed
-        try {        
-            new SattAdmin(idb).setVisible(true);
-// TODO add your handling code here:
-        } catch (InfException ex) {
-            Logger.getLogger(InloggadAdmin.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }//GEN-LAST:event_btnSattAdminActionPerformed
-
-    private void btnTaBortUtrActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTaBortUtrActionPerformed
-        try {
-            new TaBortUtrustning(idb).setVisible(true);
-            
-            // TODO add your handling code here:
-        } catch (InfException ex) {
-            Logger.getLogger(InloggadAdmin.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }//GEN-LAST:event_btnTaBortUtrActionPerformed
-
-    private void btnTaBortAgentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTaBortAgentActionPerformed
-        try {
-            // TODO add your handling code here:
-            new TaBortAgent(idb).setVisible(true);
-        } catch (InfException ex) {
-            Logger.getLogger(InloggadAdmin.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }//GEN-LAST:event_btnTaBortAgentActionPerformed
-
-    private void btnSokAgentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSokAgentActionPerformed
-        try {
-            // TODO add your handling code here:
-            new SokInfoAgent(idb).setVisible(true);
-        } catch (InfException ex) {
-            Logger.getLogger(InloggadAdmin.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }//GEN-LAST:event_btnSokAgentActionPerformed
-
-    private void btnAndraInfoAlienActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAndraInfoAlienActionPerformed
-        try {
-            // TODO add your handling code here:
-            new ÄndraInformationAlien(idb).setVisible(true);
-        } catch (InfException ex) {
-            Logger.getLogger(InloggadAdmin.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }//GEN-LAST:event_btnAndraInfoAlienActionPerformed
+        new SetOmradeschef(idb).setVisible(true);
+    }//GEN-LAST:event_btnSetOmradesChefActionPerformed
 
     private void btnRegistreraAgentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistreraAgentActionPerformed
         try {
@@ -358,19 +323,84 @@ public class InloggadAdmin extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_btnRegistreraAgentActionPerformed
 
-    private void btnSetOmradesChefActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSetOmradesChefActionPerformed
-        // TODO add your handling code here:
-        new SetOmradeschef(idb).setVisible(true);
-    }//GEN-LAST:event_btnSetOmradesChefActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnAndraInfoAlienActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAndraInfoAlienActionPerformed
         try {
             // TODO add your handling code here:
-            new ÄndraInformationAgent(idb).setVisible(true);
+            new ÄndraInformationAlien(idb).setVisible(true);
         } catch (InfException ex) {
             Logger.getLogger(InloggadAdmin.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }//GEN-LAST:event_jButton1ActionPerformed
+<<<<<<< HEAD
+    }//GEN-LAST:event_btnTaBortUtrActionPerformed
+
+    private void btnTaBortAgentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTaBortAgentActionPerformed
+        try {
+            // TODO add your handling code here:
+            new TaBortAgent(idb).setVisible(true);
+        } catch (InfException ex) {
+            Logger.getLogger(InloggadAdmin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_btnTaBortAgentActionPerformed
+=======
+    }//GEN-LAST:event_btnAndraInfoAlienActionPerformed
+>>>>>>> 09d5c225d0266fb9a76e7bfd18aafb68bb9b8140
+
+    private void btnSokAgentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSokAgentActionPerformed
+        try {
+            // TODO add your handling code here:
+            new SokInfoAgent(idb).setVisible(true);
+        } catch (InfException ex) {
+            Logger.getLogger(InloggadAdmin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_btnSokAgentActionPerformed
+
+    private void btnTaBortAgentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTaBortAgentActionPerformed
+        // TODO add your handling code here:
+        new TaBortAgent(idb).setVisible(true);
+    }//GEN-LAST:event_btnTaBortAgentActionPerformed
+
+    private void btnTaBortUtrActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTaBortUtrActionPerformed
+        try {
+            new TaBortUtrustning(idb).setVisible(true);
+
+            // TODO add your handling code here:
+        } catch (InfException ex) {
+            Logger.getLogger(InloggadAdmin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_btnTaBortUtrActionPerformed
+
+    private void btnSattAdminActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSattAdminActionPerformed
+        try {
+            new SattAdmin(idb).setVisible(true);
+            // TODO add your handling code here:
+        } catch (InfException ex) {
+            Logger.getLogger(InloggadAdmin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_btnSattAdminActionPerformed
+
+    private void btnTaBortAlienActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTaBortAlienActionPerformed
+        new TaBortAlien(idb).setVisible(true);
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnTaBortAlienActionPerformed
+
+    private void btnRegistreraAlienActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistreraAlienActionPerformed
+        try {
+            // TODO add your handling code here:
+            new RegistreraAlien(anvandarnamn, idb).setVisible(true);
+        } catch (InfException ex) {
+            Logger.getLogger(InloggadAdmin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_btnRegistreraAlienActionPerformed
+
+    private void btnAndraLosenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAndraLosenActionPerformed
+        // TODO add your handling code here:
+        new ÄndraLösenord(anvandarnamn, losenord, idb, anvandare).setVisible(true);
+    }//GEN-LAST:event_btnAndraLosenActionPerformed
+
+    private void btnSetKontorschefActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSetKontorschefActionPerformed
+        // TODO add your handling code here:
+        new setKontorschef(idb).setVisible(true);
+    }//GEN-LAST:event_btnSetKontorschefActionPerformed
 
     /**
      * @param args the command line arguments
@@ -413,6 +443,7 @@ public class InloggadAdmin extends javax.swing.JFrame {
     private javax.swing.JButton btnRegistreraAgent;
     private javax.swing.JButton btnRegistreraAlien;
     private javax.swing.JButton btnSattAdmin;
+    private javax.swing.JButton btnSetKontorschef;
     private javax.swing.JButton btnSetOmradesChef;
     private javax.swing.JButton btnSokAgent;
     private javax.swing.JButton btnTaBortAgent;
